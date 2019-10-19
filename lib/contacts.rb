6c063b77cde1a5bbb2983bfd52058db012a0e317
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-19 17:34:51 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,11 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |name, data|
+    if name == "Freddy Mercury"
+      data.each do |key, data|
+        data.shift if key == :favorite_ice_cream_flavors
+      end
+    end
+  end
 end
